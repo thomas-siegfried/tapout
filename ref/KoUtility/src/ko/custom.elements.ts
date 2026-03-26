6e23b655b5797fa6a8370b4192cbbc62f9d19e67
TSiegfried <tsiegfried@pulte.com>
2026-03-26 14:36:59 -0700

    Removed REF libraries.

--- ref/KoUtility/src/ko/custom.elements.ts
+++
@@ -1,8 +0,0 @@
-import ko from "knockout";
-(ko.bindingProvider.instance as any)["preprocessNode"] = (node: Node) => {
-  // if it contains a hyphen, we consider it to be a component
-  if (node.nodeName.includes("-")) {
-    var el: HTMLElement = node as HTMLElement;
-    el.style.display = "contents";
-  }
-};
